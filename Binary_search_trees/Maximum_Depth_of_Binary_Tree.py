e9be4f77e3ad429e5c388171d0ebffb2a10fcc7c
maratule <marattulegenov@Marats-MacBook-Pro.local>
2023-12-24 23:35:23 -0800

    First commit

--- Binary_search_trees/Maximum_Depth_of_Binary_Tree.py
+++ Binary_search_trees/Maximum_Depth_of_Binary_Tree.py
@@ -1,3 +1,10 @@
+'''Given the root of a binary tree, return its maximum depth.
+A binary tree's maximum depth is the number of nodes along the longest path
+from the root node down to the farthest leaf node.
+Input: root = [3,9,20,null,null,15,7]
+Output: 3
+'''
+
 # Definition for a binary tree node.
 # class TreeNode(object):
 #     def __init__(self, val=0, left=None, right=None):
