c7bd84278c3fa8da7cb05063564f9eb2dbd189a0
maratule <marattulegenov@Marats-MBP.attlocal.net>
2023-12-25 17:24:29 -0800

    firts commit

---
+++ Arrays/Contains_Duplicates.py
@@ -0,0 +1,28 @@
+'''Given an integer array nums, return true
+if any value appears at least twice in the array, and return false if every element is distinct.
+Example 1:
+Input: nums = [1,2,3,1]
+Output: true
+Example 2:
+Input: nums = [1,2,3,4]
+Output: false
+'''
+
+class Solution(object):
+    def containsDuplicate(self, nums):
+        """
+        :type nums: List[int]
+        :rtype: bool
+        """
+        hashmap = {}
+
+        for i in nums:
+            if i in hashmap:
+                return True
+            else:
+                hashmap[i] = 1
+        return False
+
+
+case = Solution()
+case.containsDuplicate(nums)
